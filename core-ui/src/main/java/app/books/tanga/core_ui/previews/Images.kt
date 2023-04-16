05c7da02bd5db0e1552fc95bb9a147136da7d899
Rygel Louv <quentinlouv@gmail.com>
2023-04-16 08:11:15 +0000

    Added preview for Image and Texts

---
+++ core-ui/src/main/java/app/books/tanga/core_ui/previews/Images.kt
@@ -0,0 +1,37 @@
+package app.books.tanga.core_ui.previews
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.books.tanga.core_ui.R
+import app.books.tanga.core_ui.components.ProfileImage
+import app.books.tanga.core_ui.components.SummaryImage
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF,
+    showSystemUi = true, device = "id:pixel_4a"
+)
+@Composable
+fun SummaryImagePreview() {
+    SummaryImage(
+        modifier = Modifier.size(64.dp),
+        painter = painterResource(id = R.drawable.profile_placeholder),
+        onSummaryClicked = { /*TODO*/ }
+    )
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF,
+)
+@Composable
+fun ProfilePicturePreview() {
+    ProfileImage(
+        modifier = Modifier.size(100.dp),
+        photoUrl = "https://picsum.photos/200",
+    )
+}
--- core-ui/src/main/java/app/books/tanga/core_ui/theme/Shape.kt
+++ core-ui/src/main/java/app/books/tanga/core_ui/theme/Shape.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(10.dp),
+    extraLarge = RoundedCornerShape(12.dp)
 )
