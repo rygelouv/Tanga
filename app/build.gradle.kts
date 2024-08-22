76e90a9de5fae9ddc8c01d10ff78e66d7a5cb788
Rygel Louv <quentinlouv@gmail.com>
2024-08-22 17:19:10 +0000

    New release 1.1.2 (#109)
    - Fixed favorite bug
    - Prevent player crash
    - Fixed displayName missing bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "app.books.tanga"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.1.1"
+        versionCode = 9
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
