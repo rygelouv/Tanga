f90619363c1052cc3fd47f343bd90df722b1f635
Rygel Louv <quentinlouv@gmail.com>
2025-01-06 18:56:45 +0000

    updating version and version name, in gradle and release_build.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 14
-        versionName = "1.3.0"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
